607b8743861dade71fe523eeb50ee6b0fe803643
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-18 19:18:55 +0700

    Update setup_es.py

--- helper/setup_es.py
+++ helper/setup_es.py
@@ -3,60 +3,100 @@ from elasticsearch import Elasticsearch
 
 def connect_elasticsearch():
     _es = None
-    _es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    _es = Elasticsearch([{"host": "localhost", "port": 9500}])
     if _es.ping():
-        print('Connected Elasticsearch')
+        print("Connected Elasticsearch")
     else:
-        print('Elasticsearch could not connect!')
+        print("Elasticsearch could not connect!")
     return _es
 
 
 def create_index_indicator(es_object, index_name):
     created_index_indicator = False
-    # index settings
     settings = {
         "settings": {
-            "number_of_shards": 1,
+            "number_of_shards": 2,
             "number_of_replicas": 0
         },
         "mappings": {
-            "indicator": {
-                "dynamic": "strict",
-                "properties": {
-                    "ioc_id": {
-                        "type": "text"
-                    },
-                    "ioc": {
-                        "type": "text"
-                    },
-                    "ioc_type": {
-                        "type": "text"
-                    },
-                    "created_time": {
-                        "type": "date"
-                    },
-                    "crawled_time": {
-                        "type": "date"
-                    },
-                    "source": {
-                        "type": "integer"
-                    },
-                    "calories": {
-                        "type": "nested",
-                        "properties": {
-                            "step": {"type": "text"}
-                        }
-                    },
-                }
+            "properties": {
+                "pulse_id": {
+                    "type": "text"
+                },
+                "name": {
+                    "type": "text"
+                },
+                "description": {
+                    "type": "text"
+                },
+                "author_name": {
+                    "type": "text"
+                },
+                "modified": {
+                    "type": "date"
+                },
+                "created": {
+                    "type": "date"
+                },
+                "targeted_countries": {
+                    "type": "nested",
+                    "properties": {
+                        "step": {"type": "text"}
+                    }
+                },
+                "industries": {
+                    "type": "nested",
+                    "properties": {
+                        "step": {"type": "text"}
+                    }
+                },
+                "malware_families": {
+                    "type": "nested",
+                    "properties": {
+                        "step": {"type": "text"}
+                    }
+                },
+                "attack_ids": {
+                    "type": "nested",
+                    "properties": {
+                        "step": {"type": "text"}
+                    }
+                },
+                "references": {
+                    "type": "text",
+                    "properties": {
+                        "step": {"type": "text"}
+                    }
+                },
+                "ioc_id": {
+                    "type": "text"
+                },
+                "ioc": {
+                    "type": "text"
+                },
+                "ioc_type": {
+                    "type": "text"
+                },
+                "created_time": {
+                    "type": "date"
+                },
+                "crawled_time": {
+                    "type": "date"
+                },
+                "source": {
+                    "type": "integer"
+                },
+                "category": {
+                    "type": "text",
+                },
             }
         }
     }
 
     try:
         if not es_object.indices.exists(index_name):
-            # Ignore 400 means to ignore "Index Already Exist" error.
-            es_object.indices.create(index=index_name, ignore=400, body=settings)
-            print('Created Index')
+            res = es_object.indices.create(index=index_name, ignore=400, body=settings)
+            print(res)
         created_index_indicator = True
     except Exception as ex:
         print(str(ex))
@@ -66,47 +106,42 @@ def create_index_indicator(es_object, index_name):
 
 def create_index_compromised(es_object, index_name):
     created_index_compromised = False
-    # index settings
     settings = {
         "settings": {
-            "number_of_shards": 1,
+            "number_of_shards": 2,
             "number_of_replicas": 0
         },
         "mappings": {
-            "compromised": {
-                "dynamic": "strict",
-                "properties": {
-                    "uid": {
-                        "type": "text"
-                    },
-                    "hostname": {
-                        "type": "text"
-                    },
-                    "src": {
-                        "type": "text"
-                    },
-                    "victim_hash": {
-                        "type": "text"
-                    },
-                    "creation_date": {
-                        "type": "date"
-                    },
-                    "timestamp": {
-                        "type": "date"
-                    },
-                    "country": {
-                        "type": "text",
-                    },
-                }
+            "properties": {
+                "uid": {
+                    "type": "text"
+                },
+                "hostname": {
+                    "type": "text"
+                },
+                "src": {
+                    "type": "text"
+                },
+                "victim_hash": {
+                    "type": "text"
+                },
+                "creation_date": {
+                    "type": "date"
+                },
+                "timestamp": {
+                    "type": "date"
+                },
+                "country": {
+                    "type": "text",
+                },
             }
         }
     }
 
     try:
         if not es_object.indices.exists(index_name):
-            # Ignore 400 means to ignore "Index Already Exist" error.
-            es_object.indices.create(index=index_name, ignore=400, body=settings)
-            print('Created Index')
+            res = es_object.indices.create(index=index_name, ignore=400, body=settings)
+            print(res)
         created_index_compromised = True
     except Exception as ex:
         print(str(ex))
@@ -114,14 +149,87 @@ def create_index_compromised(es_object, index_name):
         return created_index_compromised
 
 
+def create_index_samples(es_object, index_name):
+    created_index_samples = False
+    settings = {
+        "settings": {
+            "number_of_shards": 2,
+            "number_of_replicas": 0
+        },
+        "mappings": {
+            "properties": {
+                "names": {
+                    "type": "text"
+                },
+                "sha256": {
+                    "type": "text"
+                },
+                "sha1": {
+                    "type": "text"
+                },
+                "md5": {
+                    "type": "text"
+                },
+                "first_submit": {
+                    "type": "date"
+                },
+                "notification_date": {
+                    "type": "date"
+                },
+                "type_description": {
+                    "type": "text",
+                },
+                "magic": {
+                    "type": "text",
+                },
+                "country": {
+                    "type": "text",
+                },
+                "rule_name": {
+                    "type": "text",
+                },
+                "rate": {
+                    "type": "text",
+                },
+                "tags": {
+                    "type": "nested",
+                    "properties": {
+                        "step": {"type": "text"}
+                    }
+                },
+                "detected": {
+                    "type": "nested",
+                    "properties": {
+                        "step": {"type": "text"}
+                    }
+                },
+                "point": {
+                    "type": "integer",
+                }
+            }
+        }
+    }
+
+    try:
+        if not es_object.indices.exists(index_name):
+            res = es_object.indices.create(index=index_name, ignore=400, body=settings)
+            print(res)
+        created_index_samples = True
+    except Exception as ex:
+        print(str(ex))
+    finally:
+        return created_index_samples
+
+
 def store_record(elastic_object, index_name, record):
     is_stored = True
     try:
-        outcome = elastic_object.index(index=index_name, doc_type='indicator', body=record)
+        outcome = elastic_object.index(index=index_name, body=record)
         print(outcome)
     except Exception as ex:
-        print('Error in indexing data')
+        print("Error in indexing data")
         print(str(ex))
         is_stored = False
     finally:
         return is_stored
+
